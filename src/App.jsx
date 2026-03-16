877e0586158478facf76a9c4b343b7bcffda1131
Ege Crew <setup@egecrew.com>
2026-03-16 22:07:43 +0300

    Update demo link to custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -113,12 +113,12 @@ function App() {
                 <div className="demo-box">
                   <strong>🌐 Demo Web Sitesi:</strong>
                   <a 
-                    href="https://egecrew.github.io/website-dis-hekimi-dogan-kayran---724-acik-dis-hekimi/" 
+                    href="https://dis-hekimi-dogan-kayran---724-acik-dis-hekimi.egecrew.com/" 
                     target="_blank" 
                     rel="noopener noreferrer" 
                     className="demo-link"
                   >
-                    egecrew.github.io/website-dis-hekimi-dogan-kayran---724-acik-dis-hekimi
+                    dis-hekimi-dogan-kayran---724-acik-dis-hekimi.egecrew.com
                   </a>
                 </div>
               </div>
